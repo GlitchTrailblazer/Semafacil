fbbd284c099773736dc1aff2ac4679e09738dbf7
GlitchTrailblazer <143354103+GlitchTrailblazer@users.noreply.github.com>
2023-08-27 18:55:54 -0300

    Update executar.php

--- executar.php
+++ executar.php
@@ -1,22 +1,3 @@
 <?php
-$sema_verde = $_POST['sema_verde'] ?? '0';
-$sema_amarelo = $_POST['sema_amarelo'] ?? '0';
-$sema_vermelho = $_POST['sema_vermelho'] ?? '0';
-
-$semb_verde = $_POST['semb_verde'] ?? '0';
-$semb_amarelo = $_POST['semb_amarelo'] ?? '0';
-$semb_vermelho = $_POST['semb_vermelho'] ?? '0';
-
-$cmd = "python3 teste.py";
-
-if ($sema_verde || $sema_amarelo || $sema_vermelho) {
-    $cmd .= " -SEMA $sema_verde $sema_amarelo $sema_vermelho";
-}
-if ($semb_verde || $semb_amarelo || $semb_vermelho) {
-    $cmd .= " -SEMB $semb_verde $semb_amarelo $semb_vermelho";
-}
-
-exec($cmd);
-
-echo "Execução concluída";
+exec("python3 codigo.py -SEMA 5 0 0");
 ?>
