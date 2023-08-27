3f1ad9623a3911b24f4aadce8ab09ffb02800cc7
GlitchTrailblazer <143354103+GlitchTrailblazer@users.noreply.github.com>
2023-08-27 17:56:07 -0300

    Update semain.py

--- semain.py
+++ semain.py
@@ -4,50 +4,55 @@ import argparse
 import signal
 import sys
 
-# Função para acender uma luz específica
-def turn_on_light(pin, duration):
-    GPIO.output(pin, True)
-    time.sleep(duration)
-    GPIO.output(pin, False)
+# Função para ligar um LED específico
+def ligar_led(pino, duracao):
+    GPIO.output(pino, True)
+    time.sleep(duracao)
+    GPIO.output(pino, False)
 
 # Configuração dos argumentos da linha de comando
-parser = argparse.ArgumentParser(description="Control the traffic lights.")
-parser.add_argument("-semaforo", choices=["A", "B"], required=True, help="Escolher semáforo (A ou B)")
-parser.add_argument("-verde", type=int, help="Tempo para luz verde")
-parser.add_argument("-amarelo", type=int, help="Tempo para luz amarela")
-parser.add_argument("-vermelho", type=int, help="Tempo para luz vermelha")
+parser = argparse.ArgumentParser(description="Controlar os semáforos.")
+parser.add_argument("-SEMA", nargs=3, type=int, metavar=("tempo_verde", "tempo_amarelo", "tempo_vermelho"), help="Configuração do semáforo A (verde, amarelo, vermelho)")
+parser.add_argument("-SEMB", nargs=3, type=int, metavar=("tempo_verde", "tempo_amarelo", "tempo_vermelho"), help="Configuração do semáforo B (verde, amarelo, vermelho)")
 
 args = parser.parse_args()
 
-# Mapeamento dos pinos dos semáforos
-semaforo_pins = {
+# Mapeamento dos pinos dos semáforos A e B
+pino_semaforos = {
     "A": {"verde": 14, "amarelo": 15, "vermelho": 18},
     "B": {"verde": 16, "amarelo": 20, "vermelho": 21}
 }
 
-# Configuração do semáforo selecionado
-semaforo = semaforo_pins[args.semaforo]
-
-# Setup
+# Configuração
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(semaforo["verde"], GPIO.OUT)
-GPIO.setup(semaforo["amarelo"], GPIO.OUT)
-GPIO.setup(semaforo["vermelho"], GPIO.OUT)
+for semaforo in pino_semaforos.values():
+    GPIO.setup(semaforo["verde"], GPIO.OUT)
+    GPIO.setup(semaforo["amarelo"], GPIO.OUT)
+    GPIO.setup(semaforo["vermelho"], GPIO.OUT)
 
-# Turn off all lights when user ends demo
-def allLightsOff(signal, frame):
+# Desligar todas as luzes quando o usuário encerra a demonstração
+def desligar_todas_luzes(sinal, quadro):
+    for semaforo in pino_semaforos.values():
         GPIO.output(semaforo["verde"], False)
         GPIO.output(semaforo["amarelo"], False)
         GPIO.output(semaforo["vermelho"], False)
-        GPIO.cleanup()
-        sys.exit(0)
+    GPIO.cleanup()
+    sys.exit(0)
 
-signal.signal(signal.SIGINT, allLightsOff)
+signal.signal(signal.SIGINT, desligar_todas_luzes)
 
-# Acender as luzes especificadas
-if args.verde:
-    turn_on_light(semaforo["verde"], args.verde)
-if args.amarelo:
-    turn_on_light(semaforo["amarelo"], args.amarelo)
-if args.vermelho:
-    turn_on_light(semaforo["vermelho"], args.vermelho)
+# Acender as luzes especificadas em ambos os semáforos
+for semaforo, pinos in pino_semaforos.items():
+    if semaforo == "A" and args.SEMA:
+        verde, amarelo, vermelho = args.SEMA
+    elif semaforo == "B" and args.SEMB:
+        verde, amarelo, vermelho = args.SEMB
+    else:
+        continue
+    
+    if verde:
+        ligar_led(pinos["verde"], verde)
+    if amarelo:
+        ligar_led(pinos["amarelo"], amarelo)
+    if vermelho:
+        ligar_led(pinos["vermelho"], vermelho)
