2658478fff29762422c59332cd65f9cc68197282
GlitchTrailblazer <143354103+GlitchTrailblazer@users.noreply.github.com>
2023-08-27 19:14:32 -0300

    Update executar.php

--- executar.php
+++ executar.php
@@ -1,6 +1,6 @@
 <?php
 echo 'ESTOU AQUI';
-$command = escapeshellcmd('python3 teste.py -SEMA 5 0 0');
+$command = escapeshellcmd('sudo python3 teste.py -SEMA 5 0 0');
 echo 'ESTOU AQUI 1';
 $output = shell_exec($command);
 echo 'ESTOU AQUI 2!';
